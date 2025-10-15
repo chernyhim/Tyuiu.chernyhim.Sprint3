3e4e5696deade8092ff04224d8565d733fade884
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-10-15 19:04:32 +0500

    Add project files.

---
+++ Tyuiu.chernyhim.Sprint3.Task0.V27.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.chernyhim.Sprint3.Task0.V27.Lib
+{
+    public class DataService : ISprint3Task0V27
+    {
+        public double GetSumSeries(int value, int startValue, int stopValue)
+        {
+            double sum = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                sum += Math.Pow((4 / (Math.Pow(i, value))),2);
+            
+            }
+            return Math.Round(sum, 3);
+        }
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint3.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.chernyhim.Sprint3.Task0.V27.Lib;
+namespace Tyuiu.chernyhim.Sprint3.Task0.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(16, ds.GetSumSeries(1, 1, 1));
+        }
+    }
+}
