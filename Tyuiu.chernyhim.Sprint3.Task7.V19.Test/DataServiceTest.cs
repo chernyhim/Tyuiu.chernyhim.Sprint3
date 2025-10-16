265b1ee76db3e91e266ac24e1e4f06c51c7b780d
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-10-16 13:12:28 +0500

    Task7

---
+++ Tyuiu.chernyhim.Sprint3.Task7.V19.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tyuiu.chernyhim.Sprint3.Task7.V19.Lib;
+namespace Tyuiu.chernyhim.Sprint3.Task7.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startvalue = -5;
+            int stopvalue = 5;
+            int len = stopvalue - startvalue + 1;
+            double[] valueWait;
+            valueWait =new double[len];
+
+            valueWait[0] = 23.612;
+            valueWait[1] = 16.077;
+            valueWait[2] = 7.838;
+            valueWait[3] = 4.578;
+            valueWait[4] = Math.Round(2.87983, 3);
+            valueWait[5] = 0.75;
+            valueWait[6] = -4.474;
+            valueWait[7] = -9.461;
+            valueWait[8] = -7.414;
+            valueWait[9] = -6.162;
+            valueWait[10] = -7.294;
+            CollectionAssert.AreEqual(ds.GetMassFunction(-5,5),valueWait);
+        }
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint3.Task7.V19/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.chernyhim.Sprint3.Task7.V19.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
